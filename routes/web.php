2e353e18a38ad79fad623c35f97ab2b6c3f15217
Charlie Hield <charles.hield@gmail.com>
2024-06-12 19:29:31 -0500

    Remove test route

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
   return view('welcome');
 });
-
-Route::get('/test', function () {
-  $secret = 'abc123';
-
-  $s = Crypt::encryptString($secret);
-
-  return $s;
-});
